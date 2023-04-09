aed9e37a3bc9b28b273adddaf24309316a1a088e
Noor Al-Hajri <noor@Noors-MacBook-Pro.local>
2023-04-09 14:29:12 +0300

    added location controller and reviw routes and api @jehanalik @nadia-husain @nooralhajri @hajrmohamed @zahidala

--- controllers/location.js
+++ controllers/location.js
@@ -2,18 +2,28 @@ const Location = require('../models/Location')
 const Review = require('../models/Review')
 const Axios = require('axios')
 
-// GET /locations
-exports.getlocations = async (req, res) => {
-  const city = req.query.city
-  const country = req.query.country
 
-  Axios.get ('')
-
+// Get locations by city and country
+exports.getLocations = async (req, res) => {
+    const city = req.query.city;
+    const country = req.query.country;
     try {
-        const locations = await Location.find({ city: city, country: country })
-        res.status(200).json(locations)
-    } catch (error) {
-        res.status(404).json({ message: error.message })
+      const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${city},${country}&key=${process.env.GOOGLE_API_KEY}`);
+      const { lat, lng } = response.data.results[0].geometry.location;
+  
+      const { data } = await axios.get(
+        `https://travel-advisor.p.rapidapi.com/locations/search?query=${city}&lang=en_US&units=km&limit=10&offset=0&currency=USD&sort=relevance&location_id=${lat},${lng}`,
+        {
+          headers: {
+            'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+            'x-rapidapi-key': process.env.TRAVEL_ADVISOR_API_KEY,
+          },
+        }
+      );
+  
+      res.json(data.data);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server error');
     }
-
-}
+  };
