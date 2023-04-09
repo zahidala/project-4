0527a5773b2687f33f285b320f683d7c3b2a77cd
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-04-09 10:39:36 +0300

    remove flight.js from route @jehanalik @nadia-husain @nooralhajri @hajrm ohamed

--- routes/flight.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-const flightCntrl = require('../controllers/flight')
-
-router.get('/:ident', flightCntrl.flight_info_get)
-
-module.exports = router
